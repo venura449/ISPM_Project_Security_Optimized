30293720dedc240d2b3f4d6c3f916f839db4b4ab
Heyli <heylibandara@gmail.com>
2026-09-10 01:30:18 +0530

    Fixed psw git commit

--- ISPM_Project_SLIIT/backend/src/server.js
+++ ISPM_Project_SLIIT/backend/src/server.js
@@ -1,9 +1,12 @@
 const app = require('./app');
 require('dotenv').config();
+const AuthService = require('./services/auth/AuthService');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV}`);
+
+  await AuthService.createDefaultAdmin();
 });
