e413c016c789618c3b2944047e5a537b68ab8340
Heyli <heylibandara@gmail.com>
2026-09-20 02:17:41 +0530

    Add RBAC to generate password

--- ISPM_Project_SLIIT/backend/scripts/ispm_db.sql
+++ ISPM_Project_SLIIT/backend/scripts/ispm_db.sql
@@ -1001,10 +1001,23 @@ COMMIT;
 --
 -- Add password constraint
 --
-ALTER TABLE employees
-  ADD COLUMN must_change_password TINYINT(1) NOT NULL DEFAULT 0,
-  ADD COLUMN temporary_password_expires_at DATETIME NULL,
-  ADD COLUMN temporary_password_used_at DATETIME NULL;
+ALTER TABLE `employees`
+  ADD COLUMN `must_change_password` TINYINT(1) NOT NULL DEFAULT 0,
+  ADD COLUMN `temporary_password_expires_at` DATETIME NULL,
+  ADD COLUMN `temporary_password_used_at` DATETIME NULL;
+
+--
+-- Add user roles
+--
+ALTER TABLE `users`
+  ADD COLUMN `role` enum( 'admin', 'user') NOT NULL DEFAULT 'user';
+
+--
+-- Update default user role
+--
+UPDATE `users`
+SET `role` = 'admin'
+WHERE `email` = 'gaya@gmail.com';
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
